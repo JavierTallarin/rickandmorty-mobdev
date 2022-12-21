c1a9e70d73aa6a4f221f494c6b4a4f6656390eb4
javierTallarin <javier1bravo@gmail.com>
2022-12-21 16:52:20 -0300

    added initial spring-boot proyect 2.7.7 java 11

---
+++ src/test/java/com/example/rickandmortymobdev/RickandmortyMobdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rickandmortymobdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickandmortyMobdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

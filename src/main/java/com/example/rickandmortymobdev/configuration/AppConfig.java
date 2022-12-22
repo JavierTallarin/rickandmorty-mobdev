52381121b83f8f1fee492f51fb4bdc113cee1095
javierTallarin <javier1bravo@gmail.com>
2022-12-21 23:13:55 -0300

    Added configuration package Added @Bean RestTemplate

---
+++ src/main/java/com/example/rickandmortymobdev/configuration/AppConfig.java
@@ -0,0 +1,17 @@
+package com.example.rickandmortymobdev.configuration;
+
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+
+public class AppConfig {
+    @Bean
+    public RestTemplate registerRestTemplate(RestTemplateBuilder restTemplateBuilder){
+        return restTemplateBuilder.build();
+    }
+
+}

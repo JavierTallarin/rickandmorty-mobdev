9db6ca9c9c310d93ccc7279cd18ee245d3f0a7b5
javierTallarin <javier1bravo@gmail.com>
2022-12-23 13:52:27 -0300

    Add TODO: in Main

--- src/main/java/com/example/rickandmortymobdev/RickandmortyMobdevApplication.java
+++ src/main/java/com/example/rickandmortymobdev/RickandmortyMobdevApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RickandmortyMobdevApplication {
+// TODO: clases especificos metodo generic(execute)
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(RickandmortyMobdevApplication.class, args);

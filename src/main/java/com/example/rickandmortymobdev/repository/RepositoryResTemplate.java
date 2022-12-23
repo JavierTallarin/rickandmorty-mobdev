cb4ddd86669b647c62f3d4de7ab2d22fee453278
javierTallarin <javier1bravo@gmail.com>
2022-12-23 09:07:40 -0300

    Refactor RepositoryResTemplate

--- src/main/java/com/example/rickandmortymobdev/repository/RepositoryResTemplate.java
+++ src/main/java/com/example/rickandmortymobdev/repository/RepositoryResTemplate.java
@@ -2,24 +2,37 @@ package com.example.rickandmortymobdev.repository;
 
 import com.example.rickandmortymobdev.model.CharacterDTO;
 import com.example.rickandmortymobdev.model.LocationDTO;
+import com.example.rickandmortymobdev.service.IService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class RepositoryResTemplate implements IRepository{
+    @Value("${external.rickandmorty.api.character.urlBase}")
+    private String URL_BASE_CHARACTER;
+
+    @Value("${external.rickandmorty.api.location.urlBase}")
+    private String URL_BASE_LOCATION;
     private final RestTemplate restTemplate;
+
 
     public RepositoryResTemplate(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
+
     }
 
     @Override
     public CharacterDTO findCharacterById(Integer id) {
-        return null;
+        CharacterDTO characterDTO = restTemplate.getForObject(this.URL_BASE_CHARACTER.concat(id.toString()), CharacterDTO.class);
+
+        return characterDTO;
     }
 
     @Override
     public LocationDTO findLocationById(Integer id) {
-        return null;
+        LocationDTO locationDTO = restTemplate.getForObject(this.URL_BASE_LOCATION.concat(id.toString()), LocationDTO.class);
+
+        return locationDTO;
     }
 }

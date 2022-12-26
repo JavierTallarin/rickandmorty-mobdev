081bc96eb3e32939002814b05e0d9b1c306e60e6
javierTallarin <javier1bravo@gmail.com>
2022-12-26 11:40:08 -0300

    Refactor NotFoundCharacterException

--- src/main/java/com/example/rickandmortymobdev/exception/NotFoundCharacterException.java
+++ src/main/java/com/example/rickandmortymobdev/exception/NotFoundCharacterException.java
@@ -6,15 +6,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 
 @Setter
 @Getter
-public class NotFoundCharacterException extends HttpClientErrorException{
+public class NotFoundCharacterException extends RuntimeException {
     private String code;
     private String message;
 
     public NotFoundCharacterException(String code, String message) {
-        super(HttpStatus.valueOf("404"));
+        super("not found character");
         this.code = code;
         this.message = message;
     }

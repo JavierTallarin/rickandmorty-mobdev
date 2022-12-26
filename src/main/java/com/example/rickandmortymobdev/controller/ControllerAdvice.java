fa9880f4d6c8cbb15201a4da97f50c98d81dc26b
javierTallarin <javier1bravo@gmail.com>
2022-12-25 21:57:33 -0300

    Added ControllerAdvice

---
+++ src/main/java/com/example/rickandmortymobdev/controller/ControllerAdvice.java
@@ -0,0 +1,22 @@
+package com.example.rickandmortymobdev.controller;
+
+
+import com.example.rickandmortymobdev.model.ErrorDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+//https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/client/HttpClientErrorException.html
+
+@RestControllerAdvice
+public class ControllerAdvice {
+    @ExceptionHandler(value = HttpClientErrorException.class)
+    public ResponseEntity<ErrorDTO> runtimeExceptionHandler(HttpClientErrorException exception) {
+        ErrorDTO errorDTO = ErrorDTO.builder().code("404").message(exception.getMessage()).build();
+
+        return new ResponseEntity<>(errorDTO, HttpStatus.NOT_FOUND);
+
+    }
+
+}

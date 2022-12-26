b17b28d17fa901ac5cb6a6562429a239d9120e76
javierTallarin <javier1bravo@gmail.com>
2022-12-26 16:43:59 -0300

    Eliminated getter and setter and use lombok @Getter  @Setter

--- src/main/java/com/example/rickandmortymobdev/domain/CharacterResponse.java
+++ src/main/java/com/example/rickandmortymobdev/domain/CharacterResponse.java
@@ -3,10 +3,14 @@ package com.example.rickandmortymobdev.domain;
 import com.example.rickandmortymobdev.model.CharacterDTO;
 import com.example.rickandmortymobdev.model.LocationDTO;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 
 @Component
+@Getter
+@Setter
 public class CharacterResponse {
     @JsonProperty("id")
     private Integer id;
@@ -41,59 +45,4 @@ public class CharacterResponse {
         this.origin = locationDTO;
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getSpecies() {
-        return species;
-    }
-
-    public void setSpecies(String species) {
-        this.species = species;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Integer getEpisodeCount() {
-        return episodeCount;
-    }
-
-    public void setEpisodeCount(Integer episodeCount) {
-        this.episodeCount = episodeCount;
-    }
-
-    public LocationDTO getOrigin() {
-        return origin;
-    }
-
-    public void setOrigin(LocationDTO origin) {
-        this.origin = origin;
-    }
 }

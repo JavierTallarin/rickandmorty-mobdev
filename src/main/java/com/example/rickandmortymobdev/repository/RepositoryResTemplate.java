071ad1a44f34291c50fb40e3c71ef8ff9c7cbf1d
javierTallarin <javier1bravo@gmail.com>
2022-12-26 11:40:41 -0300

    Refactor important of RepositoryResTemplate

--- src/main/java/com/example/rickandmortymobdev/repository/RepositoryResTemplate.java
+++ src/main/java/com/example/rickandmortymobdev/repository/RepositoryResTemplate.java
@@ -1,16 +1,17 @@
 package com.example.rickandmortymobdev.repository;
 
+import com.example.rickandmortymobdev.exception.InvalidCharacterException;
 import com.example.rickandmortymobdev.exception.NotFoundCharacterException;
 import com.example.rickandmortymobdev.model.CharacterDTO;
 import com.example.rickandmortymobdev.model.LocationDTO;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 @Component
 public class RepositoryResTemplate implements IRepository{
@@ -29,24 +30,30 @@ public class RepositoryResTemplate implements IRepository{
     }
 
     @Override
-    public CharacterDTO findCharacterById(Integer id) {
+    public CharacterDTO findCharacterById(String id) {
 
         ResponseEntity<CharacterDTO> response = null;
 
         try {
-            response = restTemplate.exchange(this.URL_BASE_CHARACTER.concat(id.toString()), HttpMethod.GET, this.requestEntity, CharacterDTO.class);
+            response = restTemplate.exchange(this.URL_BASE_CHARACTER.concat(id), HttpMethod.GET, this.requestEntity, CharacterDTO.class);
+        }catch (HttpStatusCodeException ex){
 
-        }catch(RestClientException ex){
-            throw new NotFoundCharacterException("404", "Not found character");
+            if(ex.getStatusCode() == HttpStatus.NOT_FOUND){
+                throw  new NotFoundCharacterException("404", "not found character with id : "+String.valueOf(id));
+            }
+            if(ex.getStatusCode() == HttpStatus.INTERNAL_SERVER_ERROR) {
+                throw  new InvalidCharacterException("500", "invalid character id : "+String.valueOf(id));
+            }
+
+
         }
-
 
         return  response.getBody();
     }
 
     @Override
-    public LocationDTO findLocationById(Integer id) {
-        LocationDTO locationDTO = restTemplate.getForObject(this.URL_BASE_LOCATION.concat(id.toString()), LocationDTO.class);
+    public LocationDTO findLocationById(String id) {
+        LocationDTO locationDTO = restTemplate.getForObject(this.URL_BASE_LOCATION.concat(id), LocationDTO.class);
 
         return locationDTO;
     }
